6957482c6db8f84bece5b8b9503e0cf41231ef59
Ismail <50554815+IsmailBj@users.noreply.github.com>
2025-08-17 00:27:07 +0200

    0.8.8

--- Client/src/components/common/buttons/Buttons.tsx
+++ Client/src/components/common/buttons/Buttons.tsx
@@ -1,40 +1,5 @@
-import chardIcon from "../../../assets/icons/chardIcon.svg";
-import budgetIcon from "../../../assets/icons/moneyIcon.svg";
 import { FaCheck } from "react-icons/fa";
-import { CiSettings } from "react-icons/ci";
 import { CheckButtonProps } from "../../../types/types";
-
-export const BranchTreeBtn = () => {
-  return (
-    <a className="icon-block" href="/">
-      {/* <FaPlus /> */}
-    </a>
-  );
-};
-
-export const DashboardBtn = () => {
-  return (
-    <a className="icon-block" href="/">
-      <img src={chardIcon} alt="chard icon missing" />
-    </a>
-  );
-};
-
-export const BudgetIconBtn = () => {
-  return (
-    <a className="icon-block" href="/">
-      <img src={budgetIcon} alt="budget icon missing" />
-    </a>
-  );
-};
-
-export const SettingsBtn = () => {
-  return (
-    <a className="icon-block" href="/">
-      <CiSettings />
-    </a>
-  );
-};
 
 export const CheckButton: React.FC<CheckButtonProps> = ({
   isChecked,
